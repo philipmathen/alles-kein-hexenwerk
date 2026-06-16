59684b49fb17a905bd733a9abc955714d772cf33
philipmathen <philimat12@gmail.com>
2026-06-16 15:24:51 +0200

    conversation with claude working

--- go.mod
+++ go.mod
@@ -1,5 +1,22 @@
 module github.com/philipmathen/coding-agent
 
-go 1.23.2
+go 1.24
 
-require github.com/lpernett/godotenv v0.0.0-20230527005122-0de1d4c5ef5e // indirect
+require (
+	github.com/anthropics/anthropic-sdk-go v1.50.2
+	github.com/lpernett/godotenv v0.0.0-20230527005122-0de1d4c5ef5e
+)
+
+require (
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.2 // indirect
+	github.com/invopop/jsonschema v0.14.0 // indirect
+	github.com/pb33f/ordered-map/v2 v2.3.1 // indirect
+	github.com/standard-webhooks/standard-webhooks/libraries v0.0.1 // indirect
+	github.com/tidwall/gjson v1.18.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
+	go.yaml.in/yaml/v4 v4.0.0-rc.2 // indirect
+	golang.org/x/sync v0.16.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,93 @@
 package main
 
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/anthropics/anthropic-sdk-go"
+	"github.com/lpernett/godotenv"
+)
+
+func init() {
+	if _, err := os.Stat(".env"); err == nil {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatalf("Error loading .env file: %v", err)
+		}
+	} else {
+		log.Fatal(".env File not found")
+	}
+}
+
 func main() {
 
+	client := anthropic.NewClient()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	getUserMessage := func() (string, bool) {
+		if !scanner.Scan() {
+			return "", false
+		}
+		return scanner.Text(), true
+	}
+
+	agent := NewAgent(&client, getUserMessage)
+	err := agent.Run(context.TODO())
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+	}
+
+}
+
+func (a *Agent) Run(ctx context.Context) error {
+	conversation := []anthropic.MessageParam{}
+
+	fmt.Println("Chat with claude (ctrl + c to quit)")
+
+	for {
+		fmt.Print("\x1b[94mUser\x1b[0m: ")
+		userInput, ok := a.getUserMessage()
+		if !ok {
+			break
+		}
+		userMessage := anthropic.NewUserMessage(anthropic.NewTextBlock(userInput))
+		conversation := append(conversation, userMessage)
+
+		message, err := a.runInference(ctx, conversation)
+		if err != nil {
+			return err
+		}
+
+		for _, content := range message.Content {
+			switch content.Type {
+			case "text":
+				fmt.Printf("\x1b[93mClaude\x1b[0m: %s\n", content.Text)
+			}
+		}
+	}
+	return nil
+}
+
+func (a *Agent) runInference(ctx context.Context, conversation []anthropic.MessageParam) (anthropic.Message, error) {
+	message, err := a.client.Messages.New(ctx, anthropic.MessageNewParams{
+		Model:     anthropic.ModelClaudeHaiku4_5,
+		MaxTokens: int64(1024),
+		Messages:  conversation,
+	})
+	return *message, err
+}
+
+func NewAgent(client *anthropic.Client, getUserMessage func() (string, bool)) *Agent {
+	return &Agent{
+		client:         client,
+		getUserMessage: getUserMessage,
+	}
+}
+
+type Agent struct {
+	client         *anthropic.Client
+	getUserMessage func() (string, bool)
 }
