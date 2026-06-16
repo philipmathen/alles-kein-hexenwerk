603f31ef5d7916e41068f236fa07e109888e6a85
philipmathen <philimat12@gmail.com>
2026-06-16 16:36:58 +0200

    proper tool definition

--- go.mod
+++ go.mod
@@ -4,13 +4,13 @@ go 1.24
 
 require (
 	github.com/anthropics/anthropic-sdk-go v1.50.2
+	github.com/invopop/jsonschema v0.14.0
 	github.com/lpernett/godotenv v0.0.0-20230527005122-0de1d4c5ef5e
 )
 
 require (
 	github.com/bahlo/generic-list-go v0.2.0 // indirect
 	github.com/buger/jsonparser v1.1.2 // indirect
-	github.com/invopop/jsonschema v0.14.0 // indirect
 	github.com/pb33f/ordered-map/v2 v2.3.1 // indirect
 	github.com/standard-webhooks/standard-webhooks/libraries v0.0.1 // indirect
 	github.com/tidwall/gjson v1.18.0 // indirect
--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"bufio"
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/anthropics/anthropic-sdk-go"
+	"github.com/invopop/jsonschema"
 	"github.com/lpernett/godotenv"
 )
 
@@ -33,8 +35,8 @@ func main() {
 		}
 		return scanner.Text(), true
 	}
-
-	agent := NewAgent(&client, getUserMessage)
+	tools := []ToolDefinition{ReadFileDefinition}
+	agent := NewAgent(&client, getUserMessage, tools)
 	err := agent.Run(context.TODO())
 	if err != nil {
 		fmt.Printf("Error: %s\n", err.Error())
@@ -71,23 +73,98 @@ func (a *Agent) Run(ctx context.Context) error {
 	return nil
 }
 
+type ToolDefinition struct {
+	Name        string
+	Description string
+	InputSchema anthropic.ToolInputSchemaParam
+	Function    func(input json.RawMessage) (string, error)
+}
+
+var ReadFileDefinition = ToolDefinition{
+	Name:        "read_file",
+	Description: "Read the contents of a given relative file path. Use this when you want to see whats in the file. Don´t use it on directory names.",
+	InputSchema: ReadFileInputSchema,
+	Function:    ReadFile,
+}
+
+type ReadFileInput struct {
+	Path string `json:"path" jsonschema_description:"The realtive path of a file in the working directory"`
+}
+
+var ReadFileInputSchema = GenerateSchema[ReadFileInput]()
+
+// Function des ReadFile tools
+func ReadFile(input json.RawMessage) (string, error) {
+	// Json rohdaten kommen rein
+	readFileInput := ReadFileInput{}
+	//Json wird in das struct geldaden
+	err := json.Unmarshal(input, &readFileInput)
+	if err != nil {
+		panic(err)
+	}
+
+	//Wenn struct gültig geladen, dann File am Path lesen
+	content, err := os.ReadFile(readFileInput.Path)
+	if err != nil {
+		return "", err
+	}
+	// Inhalt des Files als string zurückgeben
+	return string(content), nil
+}
+
+// Json Schema als Vertrag mit dem LLM wie ein toolcall aussehen muss
+// jsonschema verarbeitet die jsonschema struct tags die oben im struct definert wurden
+func GenerateSchema[T any]() anthropic.ToolInputSchemaParam {
+	// Reflektor ist baut Schema aus Type (wird hier konfiguriert)
+	reflector := jsonschema.Reflector{
+		AllowAdditionalProperties: false,
+		DoNotReference:            true,
+	}
+	var v T
+	//Schema erstellen
+	schema := reflector.Reflect(v)
+	// Properties in anthropic kompatibles Schema wrappen
+	return anthropic.ToolInputSchemaParam{
+		Properties: schema.Properties,
+	}
+}
+
 func (a *Agent) runInference(ctx context.Context, conversation []anthropic.MessageParam) (anthropic.Message, error) {
+	anthropicTools := []anthropic.ToolUnionParam{}
+
+	for _, tool := range a.tools {
+		anthropicTools = append(anthropicTools, anthropic.ToolUnionParam{
+			OfTool: &anthropic.ToolParam{
+				Name:        tool.Name,
+				Description: anthropic.String(tool.Description),
+				InputSchema: tool.InputSchema,
+			},
+		})
+	}
+
 	message, err := a.client.Messages.New(ctx, anthropic.MessageNewParams{
 		Model:     anthropic.ModelClaudeHaiku4_5,
 		MaxTokens: int64(1024),
 		Messages:  conversation,
+		Tools:     anthropicTools,
 	})
 	return *message, err
 }
 
-func NewAgent(client *anthropic.Client, getUserMessage func() (string, bool)) *Agent {
+func NewAgent(
+	client *anthropic.Client,
+	getUserMessage func() (string, bool),
+	tools []ToolDefinition,
+) *Agent {
 	return &Agent{
 		client:         client,
 		getUserMessage: getUserMessage,
+		tools:          tools,
 	}
 }
 
 type Agent struct {
 	client         *anthropic.Client
 	getUserMessage func() (string, bool)
+	tools          []ToolDefinition
 }
