f7341f3b59273f7fc1cb7c9fdacd769991158e6e
philipmathen <philimat12@gmail.com>
2026-06-16 18:33:56 +0200

    List Files tool

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/anthropics/anthropic-sdk-go"
 	"github.com/invopop/jsonschema"
@@ -35,7 +37,7 @@ func main() {
 		}
 		return scanner.Text(), true
 	}
-	tools := []ToolDefinition{ReadFileDefinition}
+	tools := []ToolDefinition{ReadFileDefinition, ListFilesDefinition}
 	agent := NewAgent(&client, getUserMessage, tools)
 	err := agent.Run(context.TODO())
 	if err != nil {
@@ -49,30 +51,75 @@ func (a *Agent) Run(ctx context.Context) error {
 
 	fmt.Println("Chat with claude (ctrl + c to quit)")
 
+	readUserInput := true
 	for {
-		fmt.Print("\x1b[94mUser\x1b[0m: ")
-		userInput, ok := a.getUserMessage()
-		if !ok {
-			break
+		//Input vom User entgegennehmen
+		if readUserInput {
+			fmt.Print("\x1b[94mUser\x1b[0m: ")
+			userInput, ok := a.getUserMessage()
+			if !ok {
+				break
+			}
+			userMessage := anthropic.NewUserMessage(anthropic.NewTextBlock(userInput))
+			conversation = append(conversation, userMessage)
 		}
-		userMessage := anthropic.NewUserMessage(anthropic.NewTextBlock(userInput))
-		conversation := append(conversation, userMessage)
 
 		message, err := a.runInference(ctx, conversation)
 		if err != nil {
 			return err
 		}
 
+		conversation = append(conversation, message.ToParam())
+		toolResults := []anthropic.ContentBlockParamUnion{}
 		for _, content := range message.Content {
 			switch content.Type {
 			case "text":
 				fmt.Printf("\x1b[93mClaude\x1b[0m: %s\n", content.Text)
+			case "tool_use":
+				result := a.executeTool(content.ID, content.Name, content.Input)
+				toolResults = append(toolResults, result)
 			}
 		}
+		// Wenn es keine Ergebnisse aus den tools gibt ist der User wieder dran
+		if len(toolResults) == 0 {
+			readUserInput = true
+			continue
+		}
+		// Wenn ein Tool Ergebnisse geliefert hat, dann wird das Egebnis
+		// in den Kontext eingefügt und direkt wieder in das LLM gefüttert
+		readUserInput = false
+		conversation = append(conversation, anthropic.NewUserMessage(toolResults...))
 	}
 	return nil
 }
 
+func (a *Agent) executeTool(id, name string, input json.RawMessage) anthropic.ContentBlockParamUnion {
+	var toolDef ToolDefinition
+	var found bool
+	//verfügabre Tools des Agenten prüfen
+	for _, tool := range a.tools {
+		if tool.Name == name {
+			toolDef = tool
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		return anthropic.NewToolResultBlock(id, "tool not found", true)
+	}
+
+	fmt.Printf("\x1b[92mtool\x1b[0m: %s(%s)\n", name, input)
+	response, err := toolDef.Function(input)
+	if err != nil {
+		return anthropic.NewToolResultBlock(id, err.Error(), true)
+	}
+	fmt.Printf("\x1b[95mtool result: %s\x1b[0m\n", response)
+	return anthropic.NewToolResultBlock(id, response, false)
+}
+
+// Tools
+// ToolDefinition
 type ToolDefinition struct {
 	Name        string
 	Description string
@@ -80,6 +127,24 @@ type ToolDefinition struct {
 	Function    func(input json.RawMessage) (string, error)
 }
 
+// Json Schema als Vertrag mit dem LLM wie ein toolcall aussehen muss
+// jsonschema verarbeitet die jsonschema struct tags die oben im struct definert wurden
+func GenerateSchema[T any]() anthropic.ToolInputSchemaParam {
+	// Reflektor ist baut Schema aus Type (wird hier konfiguriert)
+	reflector := jsonschema.Reflector{
+		AllowAdditionalProperties: false,
+		DoNotReference:            true,
+	}
+	var v T
+	//Schema erstellen
+	schema := reflector.Reflect(v)
+	// Properties in anthropic kompatibles Schema wrappen
+	return anthropic.ToolInputSchemaParam{
+		Properties: schema.Properties,
+	}
+}
+
+// ReadFile
 var ReadFileDefinition = ToolDefinition{
 	Name:        "read_file",
 	Description: "Read the contents of a given relative file path. Use this when you want to see whats in the file. Don´t use it on directory names.",
@@ -112,21 +177,66 @@ func ReadFile(input json.RawMessage) (string, error) {
 	return string(content), nil
 }
 
-// Json Schema als Vertrag mit dem LLM wie ein toolcall aussehen muss
-// jsonschema verarbeitet die jsonschema struct tags die oben im struct definert wurden
-func GenerateSchema[T any]() anthropic.ToolInputSchemaParam {
-	// Reflektor ist baut Schema aus Type (wird hier konfiguriert)
-	reflector := jsonschema.Reflector{
-		AllowAdditionalProperties: false,
-		DoNotReference:            true,
+// ListFiles
+var ListFilesDefinition = ToolDefinition{
+	Name:        "list_files",
+	Description: "List files and directory at a given path. If no path is provided, lists files in the current directory",
+	InputSchema: ListFilesInputSchema,
+	Function:    ListFiles,
+}
+
+type ListFilesInput struct {
+	Path string `json:"path,omitempty" jsonschema_description:"Optional relative path to list files from. Defaults to current directory if not provided."`
+}
+
+var ListFilesInputSchema = GenerateSchema[ListFilesInput]()
+
+func ListFiles(input json.RawMessage) (string, error) {
+	listFilesInput := ListFilesInput{}
+	err := json.Unmarshal(input, &listFilesInput)
+	if err != nil {
+		panic(err)
 	}
-	var v T
-	//Schema erstellen
-	schema := reflector.Reflect(v)
-	// Properties in anthropic kompatibles Schema wrappen
-	return anthropic.ToolInputSchemaParam{
-		Properties: schema.Properties,
+	dir := "."
+	if listFilesInput.Path != "" {
+		dir = listFilesInput.Path
 	}
+
+	var files []string
+	err = filepath.WalkDir(dir, func(path string, dirEntry fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		relativePath, err := filepath.Rel(dir, path)
+		if err != nil {
+			return err
+		}
+		if relativePath != "." {
+			// versteckte Dateien ausblenden
+			fileInfo, _ := dirEntry.Info()
+			if fileInfo.Name()[0] == '.' {
+				return nil
+			}
+			if dirEntry.IsDir() {
+				files = append(files, relativePath+"/")
+			} else {
+				files = append(files, relativePath)
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	result, err := json.Marshal(files)
+	if err != nil {
+		return "", err
+	}
+	return string(result), nil
+
 }
 
 func (a *Agent) runInference(ctx context.Context, conversation []anthropic.MessageParam) (anthropic.Message, error) {
